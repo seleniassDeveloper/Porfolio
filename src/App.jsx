6928567d7e3936b06adbc1ea2a80668809d52f8d
seleniassDeveloper <seleniasanchez@MacBook-Air-de-SELENIA-2.local>
2026-07-25 18:36:07 -0400

    Add HashScrollHandler and catch-all route to guarantee no blank screens on /book-call or any anchor link

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { HashRouter, Routes, Route, useLocation } from "react-router-dom";
 import ScrollReveal from "./components/ScrollReveal";
 import "./App.css";
 import { RedesSociales } from "./components/redesSociales";
@@ -15,12 +16,29 @@ import { MasRepuestos } from "./components/ComponentesDComponentes/MasRepuestos"
 import ProyectoDashboard from "./pages/ProyectoDashboard";
 import { CalendarioAuditoria } from "./components/CalendarioAuditoria";
 
+function HashScrollHandler() {
+  const { pathname, hash } = useLocation();
+
+  useEffect(() => {
+    if (hash) {
+      const targetId = hash.replace("#", "");
+      setTimeout(() => {
+        const el = document.getElementById(targetId);
+        if (el) el.scrollIntoView({ behavior: "smooth" });
+      }, 300);
+    }
+  }, [pathname, hash]);
+
+  return null;
+}
+
 function App() {
   const { t } = useTranslation();
 
   return (
     <HashRouter>
       <RedesSociales />
+      <HashScrollHandler />
 
       <ScrollReveal baseOpacity={1} enableBlur={false} baseRotation={0} blurStrength={0}>
         <Routes>
@@ -39,6 +57,9 @@ function App() {
           <Route path="/auditoria" element={<CalendarioAuditoria />} />
           <Route path="/book-call" element={<CalendarioAuditoria />} />
           <Route path="/book" element={<CalendarioAuditoria />} />
+
+          {/* Catch-all fallback route to prevent blank screens */}
+          <Route path="*" element={<TodosLosComponentes t={t} YosiendoFeliz={YosiendoFeliz} />} />
         </Routes>
       </ScrollReveal>
     </HashRouter>
